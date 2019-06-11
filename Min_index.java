b3dcddc02bf6ac6a8cf0b1d3d094bd99c8a3e6b8
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2019-06-11 21:58:59 +0530

    Create Min_index.java

---
+++ Min_index.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Min_index
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		int q=sc.nextInt();
+		int a[]=new int[n];
+		int u,v;
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		for(int i=0;i<q;i++)
+		{
+			u=sc.nextInt();
+			v=sc.nextInt();
+			int count=0,b=a[u-1];
+			for(int j=u;j<v;j++)
+			{
+				if(b>a[j])
+				{
+					b=a[j];
+				}
+			}
+			System.out.println(b);
+		}
+	}
+}
