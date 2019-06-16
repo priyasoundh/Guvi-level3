30cc2df39cf8d3691fcbf73039686e63c5174d72
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2019-06-16 20:50:57 +0530

    Create Prefix.java

---
+++ Prefix.java
@@ -0,0 +1,46 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+public class Prefix
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		String s[]=new String[n];
+		int k=100;
+		for(int i=0;i<n;i++)
+		{
+			s[i]=sc.next();
+		}
+		for(int i=1;i<n;i++)
+		{
+			char ch[]=s[i-1].toCharArray();
+			char ch1[]=s[i].toCharArray();
+			int m=s[i-1].length();
+			int o=s[i].length();
+			if(m<=o&&m<k)
+			{
+				k=m;
+			}
+			if(m>o&&o<k)
+			{
+				k=o;
+			}
+			for(int j=0;j<k;j++)
+			{
+				
+				if(ch[j]!=ch1[j])
+				{
+					k=j;
+					j=k;
+				}
+			}
+		}
+		char ch[]=s[0].toCharArray();
+		for(int i=0;i<k;i++)
+		{
+			System.out.print(ch[i]);
+		}
+	}
+}
