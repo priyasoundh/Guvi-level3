03665b4668576c2ae6bcdcbd7bd9f960a5218818
priyasoundh <32993324+priyasoundh@users.noreply.github.com>
2019-06-16 20:56:21 +0530

    Update Prefix.java

--- Prefix.java
+++ Prefix.java
@@ -8,11 +8,11 @@ public class Prefix
 		Scanner sc=new Scanner(System.in);
 		int n=sc.nextInt();
 		String s[]=new String[n];
-		int k=100000;
 		for(int i=0;i<n;i++)
 		{
 			s[i]=sc.next();
 		}
+		int k=s[0].length();
 		for(int i=1;i<n;i++)
 		{
 			char ch[]=s[i-1].toCharArray();
